d61c864d961fa189bb7a72d95315e486bd33c0ba
BillyNgu <billy.ngn@eduge.ch>
2018-06-05 13:04:27 +0200

    add crud page of the admin.

---
+++ crud.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Auteur : Nguyen Billy
+ * Date : 2018-06-04
+ * Titre : CRUD admin
+ * Description : TPI
+ */
+require_once './dao/dao.php';
+
+$nickname = $_SESSION['user_nickname'];
+$userData = GetData($nickname);
+$crud = TRUE;
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"> 
+        <title>Blindtest</title>
+        <?php require_once './css/css_js.php'; ?>
+    </head>
+    <body>
+        <div class="container">
+            <?php require_once './navbar.php'; ?>
+        </div>
+        <script src="js/bootstrap.js" type="text/javascript"></script>
+    </body>
+</html>
