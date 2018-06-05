90ebab08c0033d1dcced9e3bf1fa3e6c93397c33
BillyNgu <billy.ngn@eduge.ch>
2018-06-05 15:59:55 +0200

    new db.

--- db/tpi.sql
+++ db/tpi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 05 juin 2018 à 11:18
+-- Généré le :  mar. 05 juin 2018 à 13:56
 -- Version du serveur :  5.7.21
 -- Version de PHP :  7.2.4
 
@@ -41,27 +41,43 @@ CREATE TABLE IF NOT EXISTS `blindtest_contains` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `choice`
+--
+
+DROP TABLE IF EXISTS `choice`;
+CREATE TABLE IF NOT EXISTS `choice` (
+  `choice_id` int(11) NOT NULL AUTO_INCREMENT,
+  `choice` varchar(50) NOT NULL,
+  `choice_is_answer` tinyint(1) NOT NULL,
+  `quizz_id` int(11) NOT NULL,
+  PRIMARY KEY (`choice_id`),
+  KEY `choice_quizz_FK` (`quizz_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `music`
 --
 
 DROP TABLE IF EXISTS `music`;
 CREATE TABLE IF NOT EXISTS `music` (
   `music_id` int(11) NOT NULL AUTO_INCREMENT,
-  `music_title` varchar(50) NOT NULL COMMENT 'Title of the song',
-  `music_description` varchar(50) NOT NULL COMMENT 'Description of the song',
-  `music_file` text NOT NULL COMMENT 'The directory of the file',
-  `music_cover` blob NOT NULL COMMENT 'The cover of the song',
+  `music_title` varchar(50) NOT NULL,
+  `music_description` varchar(50) NOT NULL,
+  `music_file` text NOT NULL,
+  `music_cover` text NOT NULL,
   PRIMARY KEY (`music_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `music_style`
+-- Structure de la table `music_type`
 --
 
-DROP TABLE IF EXISTS `music_style`;
-CREATE TABLE IF NOT EXISTS `music_style` (
+DROP TABLE IF EXISTS `music_type`;
+CREATE TABLE IF NOT EXISTS `music_type` (
   `music_style_id` int(11) NOT NULL AUTO_INCREMENT,
   `music_style` varchar(50) NOT NULL,
   `music_id` int(11) NOT NULL,
@@ -78,9 +94,9 @@ CREATE TABLE IF NOT EXISTS `music_style` (
 DROP TABLE IF EXISTS `parameters`;
 CREATE TABLE IF NOT EXISTS `parameters` (
   `parameters_id` int(11) NOT NULL AUTO_INCREMENT,
-  `parameters_time` int(11) NOT NULL COMMENT 'The time in second',
-  `parameters_questions` int(11) NOT NULL COMMENT 'The number of questions',
-  `parameters_type` int(11) NOT NULL COMMENT '1 -> image, 2 -> song, 3 -> both',
+  `parameters_time` int(11) NOT NULL,
+  `parameters_questions_number` int(11) NOT NULL,
+  `parameters_type` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`parameters_id`),
   KEY `parameters_users_FK` (`user_id`)
@@ -95,8 +111,9 @@ CREATE TABLE IF NOT EXISTS `parameters` (
 DROP TABLE IF EXISTS `quizz`;
 CREATE TABLE IF NOT EXISTS `quizz` (
   `quizz_id` int(11) NOT NULL AUTO_INCREMENT,
-  `quizz_date` date NOT NULL COMMENT 'Date of the day of the quizz',
-  `quizz_score` int(11) NOT NULL COMMENT 'The score got during the quizz',
+  `quizz_date` date NOT NULL,
+  `quizz_score` int(11) NOT NULL,
+  `quizz_question` varchar(50) NOT NULL,
   `parameters_id` int(11) NOT NULL,
   PRIMARY KEY (`quizz_id`),
   KEY `quizz_parameters_FK` (`parameters_id`)
@@ -111,24 +128,15 @@ CREATE TABLE IF NOT EXISTS `quizz` (
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(50) NOT NULL COMMENT 'Name of the user',
-  `user_nickname` varchar(50) NOT NULL COMMENT 'Nickname of the user',
-  `user_email` varchar(50) NOT NULL COMMENT 'Email of the user',
-  `user_password` varchar(50) NOT NULL COMMENT 'Password of the user',
-  `user_profilepic` text NOT NULL COMMENT 'Profile picture of the user (dir)',
+  `user_name` varchar(50) NOT NULL,
+  `user_nickname` varchar(50) NOT NULL,
+  `user_email` varchar(50) NOT NULL,
+  `user_password` varchar(50) NOT NULL,
+  `user_profilepic` text,
   `user_status` tinyint(1) NOT NULL,
   PRIMARY KEY (`user_id`),
-  UNIQUE KEY `user_nickname` (`user_nickname`,`user_email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `users`
---
-
-INSERT INTO `users` (`user_id`, `user_name`, `user_nickname`, `user_email`, `user_password`, `user_profilepic`, `user_status`) VALUES
-(1, 'root', 'root', 'root@admin.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 'root-WWDTM_logo_clr_stacked_highres.jpg', 1),
-(2, 'guess', 'guess', 'guess@guess.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 'guess-blindtest.png', 0),
-(3, 'test', 'test', 'test@test.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 'test-loginformatique_dir_couleur.png', 0);
+  UNIQUE KEY `user_nickname` (`user_nickname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Contraintes pour les tables déchargées
@@ -142,9 +150,15 @@ ALTER TABLE `blindtest_contains`
   ADD CONSTRAINT `blindtest_contains_quizz_FK` FOREIGN KEY (`quizz_id`) REFERENCES `quizz` (`quizz_id`);
 
 --
--- Contraintes pour la table `music_style`
+-- Contraintes pour la table `choice`
 --
-ALTER TABLE `music_style`
+ALTER TABLE `choice`
+  ADD CONSTRAINT `choice_quizz_FK` FOREIGN KEY (`quizz_id`) REFERENCES `quizz` (`quizz_id`);
+
+--
+-- Contraintes pour la table `music_type`
+--
+ALTER TABLE `music_type`
   ADD CONSTRAINT `music_type_music_FK` FOREIGN KEY (`music_id`) REFERENCES `music` (`music_id`);
 
 --
