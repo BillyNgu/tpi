c5c4b8612583f473f5f2da5b6097a87468cbbe82
BillyNgu <billy.ngn@eduge.ch>
2018-06-05 14:45:57 +0200

    add visual for parameter page.

--- param.php
+++ param.php
@@ -10,7 +10,6 @@ require_once './dao/dao.php';
 $nickname = $_SESSION['user_nickname'];
 $userData = GetData($nickname);
 $param = TRUE;
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -22,7 +21,50 @@ $param = TRUE;
     <body>
         <div class="container">
             <?php require_once './navbar.php'; ?>
+            <form action="param.php" method="post">
+                <fieldset>
+                    <legend>Paramètre</legend>
+                    <p>Temps : </p>
+                    <div class="slidecontainer">
+                        <input type="range" min="10" max="100" value="30" class="slider" id="myRange">
+                        <p><span id="demo"></span> seconde(s).</p>
+                    </div>
+                    <select>
+                        <option value="5">5 questions</option>
+                        <option value="10">10 questions</option>
+                        <option value="15">15 questions</option>
+                    </select>
+                    <table>
+                        <tr>
+                            <th colspan="2">Type de questions : </th>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label>
+                                    <input type="radio" name="question" value="1" checked="checked" />Chant
+                                </label>
+                            </td>
+                            <td>
+                                <label>
+                                    <input type="radio" name="question" value="2" />Image
+                                </label>
+                            </td>
+                        </tr>
+                    </table>
+                    <button class="btn btn-primary" type="submit">Enregistrer</button>
+                </fieldset>
+            </form>
         </div>
+        <script>
+            var slider = document.getElementById("myRange");
+            var output = document.getElementById("demo");
+            output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+            slider.oninput = function () {
+                output.innerHTML = this.value;
+            };
+        </script>
         <script src="js/bootstrap.js" type="text/javascript"></script>
     </body>
 </html>
