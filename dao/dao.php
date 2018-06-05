8c78011ef4084a1e545efb262853215c103cf521
BillyNgu <billy.ngn@eduge.ch>
2018-06-05 09:03:15 +0200

    add visual to profile page.

--- dao/dao.php
+++ dao/dao.php
@@ -11,109 +11,13 @@ require_once './dao/connectionBase.php';
 require_once './dao/flashmessage.php';
 
 /**
- * Get the list of all Pokemon
- * @return array
+ * Create a user
+ * @param type $name name of the user
+ * @param type $nickname nickname of the user
+ * @param type $email email of the user
+ * @param type $pwd password of the user
+ * @param type $profilepic profile picture of the user
  */
-function getAllPokemon() {
-    $sql = "SELECT `pokemonId`, `pokemonSprite`, `pokemonName`  FROM `pokemon`";
-    $query = pokedb()->prepare($sql);
-    $query->execute();
-    return $result = $query->fetchAll(PDO::FETCH_ASSOC);
-}
-
-/**
- * Get all the types
- * @return array
- */
-function getAllTypes() {
-    $sql = "SELECT `typeImage`, `typeName` FROM `type` ORDER BY `typeName` ASC";
-    $query = pokedb()->prepare($sql);
-    $query->execute();
-    return $result = $query->fetchAll(PDO::FETCH_ASSOC);
-}
-
-function getAllTypeImages() {
-    $sql = "SELECT `typeImage` FROM `type` ORDER BY `type`.`typeName` ASC";
-    $query = pokedb()->prepare($sql);
-    $query->execute();
-    return $result = $query->fetchAll(PDO::FETCH_ASSOC);
-}
-
-function getStrengthFactor($cpt) {
-//    $sql = "SELECT * FROM `weakness` ORDER BY `weakness`.`defendTypeId`, `weakness`.`attackTypeId` ASC";
-//    $sql = "SELECT `weakness`.`strengthFactor` FROM `weakness`, `type` WHERE `weakness`.`defendTypeId` = (SELECT `typeId` FROM `type` WHERE `type`.`typeImage` = :typeImage) LIMIT 18";
-    $sql = "SELECT * FROM `weakness`, `type` WHERE `weakness`.`defendTypeId` = :cpt and attackTypeId = type.typeId ORDER BY type.typeName limit 18";
-    $query = pokedb()->prepare($sql);
-    $query->bindParam(':cpt', $cpt, PDO::PARAM_INT);
-    $query->execute();
-    return $result = $query->fetchAll(PDO::FETCH_ASSOC);
-}
-
-/**
- * Get all attacks
- * @return array
- */
-function getAllAttack() {
-    $sql = "SELECT `moveName`, `movePower`, `moveAccuracy`, `typeId` FROM `move`";
-    $query = pokedb()->prepare($sql);
-    $query->execute();
-    return $result = $query->fetchAll(PDO::FETCH_ASSOC);
-}
-
-/**
- * Get a type by its id
- * @param int $idType The type id
- * @return string
- */
-function getTypeById($idType) {
-    $sql = "SELECT `typeImage` FROM `type` WHERE `typeId` = :idType";
-    $query = pokedb()->prepare($sql);
-    $query->bindParam(':idType', $idType, PDO::PARAM_INT);
-    $query->execute();
-    return $query->fetch()[0];
-}
-
-/**
- * Get all types of a selected Pokemon
- * @param int $idPokemon
- * @return array
- */
-function getPokemonType($idPokemon) {
-    $sql = "SELECT `type`.`typeId`, `type`.`typeImage`"
-            . "FROM `type`, `composed` WHERE `type`.`typeId` = `composed`.`typeId` AND `composed`.`pokemonId` = :idPokemon";
-    $query = pokedb()->prepare($sql);
-    $query->bindParam(':idPokemon', $idPokemon, PDO::PARAM_INT);
-    $query->execute();
-    return $query->fetchAll();
-}
-
-/**
- * Get all types of attacks
- * @param int $idMove The move Id
- * @return array
- */
-function getAttackType($idMove) {
-    $sql = "SELECT `typeImage` FROM `type`, `move` WHERE `type`.`typeId`=`move`.`typeId` AND `move`.`moveId`= :idMove";
-    $query = pokedb()->prepare($sql);
-    $query->bindParam(':idMove', $idMove, PDO::PARAM_INT);
-    $query->execute();
-    return $query->fetchAll(PDO::FETCH_ASSOC);
-}
-
-/**
- * Get description of a Pokemon
- * @param int $idPokemon The Pokemon id
- * @return array
- */
-function getDescription($idPokemon) {
-    $sql = "SELECT`pokemonId`, `pokemonName`, `pokemonDescription`, `pokemonImg`, `pokemonSprite` FROM `pokemon` WHERE `pokemonId` = :idPokemon";
-    $query = pokedb()->prepare($sql);
-    $query->bindParam(':idPokemon', $idPokemon, PDO::PARAM_INT);
-    $query->execute();
-    return $query->fetch(PDO::FETCH_ASSOC);
-}
-
-
 function CreateUser($name, $nickname, $email, $pwd, $profilepic) {
     $sql = "INSERT INTO `users`(`user_name`, `user_nickname`, `user_email`, `user_password`, `user_profilepic`, `user_status`) "
             . "VALUES (:name, :nickname, :email, :pwd, :profilepic, 0)";
@@ -153,10 +57,14 @@ function CheckLogin($nickname, $pwd) {
     }
 }
 
-function getSaltFromUser($Nickname) {
-    $sql = "SELECT `userSalt` from `user` WHERE `userNickname` = :Nickname";
-    $query = pokedb()->prepare($sql);
-    $query->bindParam(':Nickname', $Nickname, PDO::PARAM_STR);
+/**
+ * Get data from db with nickname
+ * @param type $nickname nickname of the user
+ */
+function GetData($nickname){
+    $sql = "SELECT `user_name`, `user_email`, `user_profilepic` FROM `users` WHERE `user_nickname` = :nickname";
+    $query = pdo()->prepare($sql);
+    $query->bindParam(':nickname', $nickname, PDO::PARAM_STR);
     $query->execute();
-    return $query->fetch()[0];
+    return $query->fetch(PDO::FETCH_ASSOC);
 }
--- profile.php
+++ profile.php
@@ -10,6 +10,7 @@ require_once './dao/dao.php';
 $nickname = $_SESSION['user_nickname'];
 $profile = TRUE;
 
+$userData = GetData($nickname);
 ?>
 <!DOCTYPE html>
 <html>
@@ -21,6 +22,26 @@ $profile = TRUE;
     <body>
         <div class="container">
             <?php require_once './navbar.php'; ?>
+            <div class="row mt-3">
+                <div class="col-md-auto">
+                    <div class="card" style="width: 18rem;">
+                        <?php if (!empty($userData['user_profilepic'])): ?>
+                            <img class="card-img-top" src="./uploaded_files/img/<?php echo $userData['user_profilepic']; ?>">
+                        <?php else: ?>
+                            <img class="card-img-top" src="./uploaded_files/img/no-avatar.png">
+                        <?php endif; ?>
+                    </div>
+                </div>
+                <div class="col">
+                    <h3>Pseudo : <?php echo $nickname ?></h3>
+                    <h5>Nom : <?= $userData['user_name'] ?> </h5> <br>
+                </div>
+                <div class="col">
+                    <fieldset>
+                        <legend>Score</legend>
+                    </fieldset>
+                </div>
+            </div>
         </div>
         <script src="js/bootstrap.js" type="text/javascript"></script>
     </body>
