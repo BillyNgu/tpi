7f0556798bd16181136a6010127609ade2a79b8b
BillyNgu <billy.ngn@eduge.ch>
2018-06-06 13:04:27 +0200

    update crud functions.

--- dao/dao.php
+++ dao/dao.php
@@ -109,26 +109,87 @@ function UpdateProfilePicture($nickname, $picture, $old_picture) {
     }
 }
 
-
 function Add_Question($question) {
     $sql = "INSERT INTO `quizz`(`quizz_question`) VALUES (:question)";
+    $query = pdo()->prepare($sql);
+    $query->bindParam(':question', $question, PDO::PARAM_STR);
+    $query->execute();
 }
 
+/**
+ * Get all information of the last record inserted
+ * @return type array
+ */
+function Get_last_question() {
+    $sql = "SELECT * FROM `quizz` WHERE `quizz_id` = (SELECT MAX(`quizz_id`) FROM `quizz`)";
+    $query = pdo()->prepare($sql);
+    $query->execute();
+    return $query->fetch(PDO::FETCH_ASSOC);
+}
 
 function Add_Choice($choice1, $choice2, $choice3, $choice4, $answer, $question_id) {
     $sql = "INSERT INTO `choice`(`choice`, `choice_is_answer`, `quizz_id`) "
-            . "VALUES ([value-1],[value-2],[value-3])";
-    
-    $choice = [$choice1, $choice2, $choice3, $choice4];
-}
+            . "VALUES (:choice, :answer, :question_id)";
+    $query = pdo()->prepare($sql);
 
+    $query->bindParam(':choice1', $choice1, PDO::PARAM_STR);
+    $query->bindParam(':choice2', $choice2, PDO::PARAM_STR);
+    $query->bindParam(':choice3', $choice3, PDO::PARAM_STR);
+    $query->bindParam(':choice4', $choice4, PDO::PARAM_STR);
+
+
+
+//    $query->bindParam(':answer', $answer, PDO::PARAM_INT);
+    $query->bindParam(':question_id', $question_id, PDO::PARAM_INT);
+
+    $choice = [':choice1', ':choice2', ':choice3', ':choice4'];
+
+    for ($index = 0; $index < count($choice); $index++) {
+        switch ($answer) {
+            case 1:
+                $answer = 1;
+                break;
+            case 2:
+                $answer = 1;
+                break;
+            case 3:
+                $answer = 1;
+                break;
+            case 4:
+                $answer = 1;
+                break;
+            default:
+                $answer = 0;
+                break;
+        }
+        $query->bindParam(':answer', $answer, PDO::PARAM_INT);
+        $query->bindParam(':choice', $choice[$index], PDO::PARAM_STR);
+        $query->execute();
+    }
+}
 
 function Add_Music($music_title, $music_description, $music_file, $music_cover) {
     $sql = "INSERT INTO `music`(`music_title`, `music_description`, `music_file`, `music_cover`) "
-            . "VALUES ([value-1],[value-2],[value-3],[value-4])";
+            . "VALUES (:music_title, :music_description, :music_file, :music_cover)";
+    $query = pdo()->prepare($sql);
+    $query->bindParam(':music_title', $music_title, PDO::PARAM_STR);
+    $query->bindParam(':music_description', $music_description, PDO::PARAM_STR);
+    $query->bindParam(':music_file', $music_file, PDO::PARAM_STR);
+    $query->bindParam(':music_cover', $music_cover, PDO::PARAM_STR);
+    $query->execute();
 }
 
+function Get_last_music() {
+    $sql = "SELECT * FROM `music` WHERE `music_id` = (SELECT MAX(`music_id`) FROM `music`)";
+    $query = pdo()->prepare($sql);
+    $query->execute();
+    return $query->fetch(PDO::FETCH_ASSOC);
+}
 
 function Add_Music_Style($style, $music_id) {
-    
+    $sql = "INSERT INTO `music_style`(`music_style`, `music_id`) VALUES (:style, :music_id)";
+    $query = pdo()->prepare($sql);
+    $query->bindParam(':style', $style, PDO::PARAM_STR);
+    $query->bindParam(':music_id', $music_id, PDO::PARAM_INT);
+    $query->execute();
 }
