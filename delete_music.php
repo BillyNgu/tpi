ec454f3f1a847f34fb9078e7587a3d91956cb8c6
BillyNgu <billy.ngn@eduge.ch>
2018-06-07 10:21:15 +0200

    add delete a music file.

---
+++ delete_music.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Auteur : Nguyen Billy
+ * Date : 2018-06-04
+ * Titre : Supression
+ * Description : TPI
+ */
+require_once './dao/dao.php';
+
+$music_id = filter_input(INPUT_GET, 'music_id', FILTER_VALIDATE_INT);
+$music = Get_music($music_id);
+
+
+Delete_music($music_id, $music['music_cover'], $music['music_file']);
+
+
+header("Location:crud_option.php");
