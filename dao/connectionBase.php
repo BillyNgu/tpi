4923d712173ad517ac1f3a5120bfbf66ba005491
BillyNgu <billy.ngn@eduge.ch>
2018-06-12 08:42:11 +0200

    update comment + indent.

--- dao/connectionBase.php
+++ dao/connectionBase.php
@@ -1,21 +1,23 @@
 <?php
+
 /*
  * Auteur : Nguyen Billy
  * Date : 2018-06-04
+ * Description : script of connection to db
  */
 
- function pdo() {
-            static $dbc = null;
+function pdo() {
+    static $dbc = null;
 
-            if ($dbc == null) {
-                try {
-                    $dbc = new PDO('mysql:host=' . DB_HOST /*. DB_PORT*/ . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
-                        PDO::ATTR_PERSISTENT => true));
-                } catch (Exception $e) {
-                    echo 'Erreur : ' . $e->getMessage() . '<br />';
-                    echo 'N° : ' . $e->getCode() . '<br />';
-                    die('Could not connect to MySql');
-                }
-            }
-            return $dbc;
+    if ($dbc == null) {
+        try {
+            $dbc = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+                PDO::ATTR_PERSISTENT => true));
+        } catch (Exception $e) {
+            echo 'Erreur : ' . $e->getMessage() . '<br />';
+            echo 'N° : ' . $e->getCode() . '<br />';
+            die('Could not connect to MySql');
         }
+    }
+    return $dbc;
+}
